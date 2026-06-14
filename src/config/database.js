454e91a2a91fdb414767c5568ede33a40acb7896
Goldie3 <brunoaltamirano978@gmail.com>
2026-06-14 18:06:41 -0400

    modificado para funcionamiento en railway

--- src/config/database.js
+++ src/config/database.js
@@ -18,11 +18,11 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_PROD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     dialect: 'mysql',
   },
 };
